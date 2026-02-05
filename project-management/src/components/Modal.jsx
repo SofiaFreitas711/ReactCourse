4582a71dda197e3168321a9d4e206860ceb0ab0f
SofiaFreitas711 <anasofiafreitas711@gmail.com>
2026-02-05 22:38:31 +0000

    Project management app - UseRef, ForwardRef, Portals, UseSate

---
+++ project-management/src/components/Modal.jsx
@@ -0,0 +1,25 @@
+import { forwardRef, useImperativeHandle, useRef } from 'react'
+import {createPortal} from 'react-dom'
+import Button from './Button.jsx'
+
+const Modal = forwardRef(function Modal({children, buttonCaption}, ref){
+    const dialogRef = useRef()
+    useImperativeHandle(ref, ()=>{
+        return{
+            open(){
+                dialogRef.current.showModal()
+            }
+        }
+    })
+    return createPortal(
+        <dialog ref={dialogRef} className='backdrop:bg-stone-900/90 p-4 rounded-md hadow-md'>
+            {children}
+            <form method='dialog' className='mt-4 text-right'>
+                <Button>{buttonCaption}</Button>
+            </form>
+        </dialog>,
+        document.querySelector("#modal-root")
+    )
+})
+
+export default Modal
